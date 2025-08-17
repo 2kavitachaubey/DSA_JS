b244fdfd1494a74a510e5636a8891e96eb39bb56
2kavitachaubey <chaubeykavita750@gmail.com>
2025-08-17 23:40:43 +0530

    code

---
+++ leetCode/addFourNum.js
@@ -0,0 +1,13 @@
+let arr = [1,5,7,2,4,3];
+for(let i = 0 ; i<arr.length; i++){
+    for(let j = i+1 ; j<arr.length; j++){
+        if(arr[i]>arr[j]){
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
+let sum = arr[0] + arr[1] + arr[2] + arr[3];
+let sum1 =arr[2]+ arr[3] + arr[4] + arr[5];
+console.log(sum + " " + sum1);
